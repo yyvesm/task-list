227b7ab5195bacd26693484d619257f73335d958
yves <yvesmomplais@hotmail.com>
2021-09-29 22:26:52 -0400

    added main components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import Task from './components/Task';
+import TaskList from './components/TaskList';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
---
+++ src/components/TaskList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function TaskList(props) {
+    return 
+}
+
+export default TaskList
